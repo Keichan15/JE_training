3aed943198626ee8e3e437363d5e7cd3565eae80
keichan15 <keisuke1515gaming@outlook.jp>
2023-09-17 00:09:01 +0900

    devise ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,4 +62,12 @@ class User < ApplicationRecord
     end
   end
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = "ゲストユーザー"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # ゲストログイン用ルーティングの追加
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
 
